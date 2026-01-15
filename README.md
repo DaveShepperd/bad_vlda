db417af1e94fd676346492ff5533b1ad0b3f641d
Dave Shepperd <dshepperd7@gmail.com>
2026-01-15 14:15:59 -0800

    Added license, some text to README.md and fixed the record check.

---
+++ README.md
@@ -0,0 +1,9 @@
+# bad_vlda
+
+Simple program to scan a file to see if it has a record structure
+according to the macxx/llf binary record structures of ob, lb and vlda.
+
+This structure is a 2 byte record count, little endian. The count can
+never be less than 1 nor more than 16383. If the count does not include
+itself. If the count is odd, then add one to the count to get to the
+start of the next record.
--- bad_vlda.c
+++ bad_vlda.c
@@ -1,19 +1,25 @@
 /*
-        badvlda.c - Program to look through files to see if there is a record
-	structure within.
+MIT License
 
-        This program is free software: you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation, either version 3 of the License, or
-        (at your option) any later version.
+Copyright (c) 2026 Dave Shepperd
 
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-        You should have received a copy of the GNU General Public License
-        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #include <stdio.h>
@@ -35,9 +41,12 @@ static int walkBuf(uint8_t *buf, size_t len)
 	{
 		recLen = (bp[1] << 8) | bp[0];
 		pad = recLen&1;
-		if ( recLen < 3 || recLen >= 16384 )
+		if ( recLen < 1 || recLen >= 16384 )
 			return 1;			/* failed */
-		bp = bp + 2 + recLen + pad;
+		recLen += 2+pad;
+		if (recLen > len )
+			return 1;
+		bp += recLen;
 	}
 	return 0;
 }
